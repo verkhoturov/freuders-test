1a4d5e150eacfe4bfdd0276c1d5f423600bf8662
VerkhoturovIP <verkhoturovip@sibur.ru>
2024-09-29 16:45:26 +0300

    uodate github action

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => {
     const BACKEND_API_URL = env.VITE_BACKEND_API_URL;
     return {
         plugins: [react()],
+        base: "https://verkhoturov.github.io/freuders-test/",
         css: {
             preprocessorOptions: {
                 scss: {
